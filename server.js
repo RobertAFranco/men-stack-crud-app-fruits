4269b50ce3529615f15e8130cf38dd1e0e8fed82
RobertAFranco <bigbobontheradio@gmail.com>
2024-07-30 13:13:42 -0400

    set up database

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 // Here is where we import modules
 // We begin by loading Express
-import express from 'express'
+import express from "express"
+import dotenv from "dotenv" // require package
+import mongoose from "mongoose" 
 
+dotenv.config(); // Loads the environment variables from .env file
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+
 // GET /
 app.get("/", async (req, res) => {
     res.render("index.ejs");
-  });
-   
-
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
 });
+
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
+    app.listen(3000, () => {
+        console.log("Listening on port 3000");
+        });
+})
