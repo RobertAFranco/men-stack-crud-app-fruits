bfe41ddf58e1af0f6cd3196c74b2658212daaefb
RobertAFranco <bigbobontheradio@gmail.com>
2024-07-30 14:04:47 -0400

    added cluster

--- server.js
+++ server.js
@@ -3,11 +3,14 @@
 import express from "express"
 import dotenv from "dotenv" // require package
 import mongoose from "mongoose" 
+import Fruit from "./models/fruit.js"
 
 dotenv.config(); // Loads the environment variables from .env file
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI);
+
+app.use(express.urlencoded({ extended: false }));
 
 
 // GET /
@@ -15,9 +18,30 @@ app.get("/", async (req, res) => {
     res.render("index.ejs");
 });
 
+
+// GET /fruits/new
+app.get("/fruits/new", async (req, res) => {
+    res.render("fruits/new.ejs");
+  });
+
+
+
+// POST /fruits
+app.post("/fruits", async (req, res) => {
+    if (req.body.isReadyToEat === "on") {
+      req.body.isReadyToEat = true;
+    } else {
+      req.body.isReadyToEat = false;
+    }
+    await Fruit.create(req.body);
+    res.redirect("/fruits/new");
+  });
+  
+  
+
 mongoose.connection.on("connected", () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
-    app.listen(3000, () => {
+    app.listen(4000, () => {
         console.log("Listening on port 3000");
         });
 })
